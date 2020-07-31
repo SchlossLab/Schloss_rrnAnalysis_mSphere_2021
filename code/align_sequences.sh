1ad3b59f8206cb0b7eeb4979e0669f135be344e0
Pat Schloss <pschloss@umich.edu>
2020-07-31 16:20:47 -0400

    Modify sequence heaader to remove spaces, closes #15

--- code/align_sequences.sh
+++ code/align_sequences.sh
@@ -8,5 +8,16 @@
 # We need to include flip=T to make sure all sequences are pointed in the same
 # direction
 
-code/mothur/mothur '#align.seqs(fasta=data/raw/rrnDB-5.6_16S_rRNA.fasta,
+sed "s/ /_/g" data/raw/rrnDB-5.6_16S_rRNA.fasta > data/raw/rrnDB-5.6_16S_rRNA.temp.fasta
+
+code/mothur/mothur '#align.seqs(fasta=data/raw/rrnDB-5.6_16S_rRNA.temp.fasta,
 reference=data/references/silva_seed/silva.seed_v138.align, flip=T)'
+
+if [[ $? -eq 0 ]]
+then
+mv data/raw/rrnDB-5.6_16S_rRNA.temp.align data/raw/rrnDB-5.6_16S_rRNA.align
+rm data/raw/rrnDB-5.6_16S_rRNA.temp.fasta
+else
+echo "FAIL: mothur failed to align sequences"
+exit 1
+fi
